9703155e9cbf27ff8249cc9a380096bb7c4533da
Damian <damian.strony@gmail.com>
2013-08-14 23:33:28 +0200

    init commit with Twitter Bootstrap

---
+++ index.php
@@ -0,0 +1,14 @@
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <title>Event Calendar</title>
+    <meta name="author" content="Damian Szymczuk">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="js/bootstrap.min.js" rel="stylesheet">
+</head>
+<body>
+
+
+
+</body>
+</html>
